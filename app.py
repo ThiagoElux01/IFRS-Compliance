33ed2c48da1d3e2cdd5299c0c154f67008c03391
ThiagoElux01 <thiago.farias@electrolux.com>
2026-03-26 10:12:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ import streamlit as st
 st.write("URL:", st.secrets.get("SUPABASE_URL"))
 st.write("KEY:", st.secrets.get("SUPABASE_KEY"))
 from datetime import datetime
-from supabase import create_client
+from supabase import create_client, Client
+
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+
+supabase: Client = create_client(url, key)
+
 import pytz
 
 # --- CONFIG STREAMLIT ---
