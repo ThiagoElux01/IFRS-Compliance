2f9272c508f9cd3ad394754c5e8d9458504fb288
ThiagoElux01 <thiago.farias@electrolux.com>
2026-03-26 11:10:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,23 +19,23 @@ col1, col2 = st.columns([1, 1])
 # =====================================================================
 with col1:
     st.markdown("## IFRS")
-    st.write("Insira uma mensagem abaixo:")
+    st.write("Insira uma norma abaixo:")
 
-    # CAMPO TEXTO 1
-    msg = st.Norma_input("Mensagem:")
+    # CAMPO NORMA
+    norma = st.text_input("Norma:")
 
     # BOTÃO PARA INSERIR TEXTO 1
-    if st.button("Enviar"):
-        if msg.strip():
+    if st.button("Enviar Norma"):
+        if norma.strip():
             brasil_tz = pytz.timezone("America/Sao_Paulo")
             now = datetime.now(brasil_tz)
 
             supabase.table("messages").insert({
-                "Norma": msg,
+                "Norma": norma,
                 "created_at": now.isoformat()
             }).execute()
 
-            st.success("Mensagem enviada!")
+            st.success("Norma enviada!")
             st.rerun()
 
     st.markdown("---")
@@ -48,12 +48,12 @@ with col1:
     lista_ids = {f"{row['id']} - {row['Norma']}": row['id'] for row in existing.data}
 
     # Selectbox COM KEY (evita reset na rerender)
-    selected = st.selectbox("Escolha uma mensagem existente:", lista_opcoes, key="select_msg")
+    selected = st.selectbox("Escolha uma Norma existente:", lista_opcoes, key="select_norma")
 
     # GUARDA O ID NA SESSION_STATE
     st.session_state["selected_id"] = lista_ids[selected]
 
-    texto2 = st.Norma_input("Texto 2:")
+    texto2 = st.text_input("Texto 2:")
 
     if st.button("Salvar Texto 2"):
         if texto2.strip():
@@ -71,7 +71,7 @@ with col1:
 # COLUNA DIREITA
 # =====================================================================
 with col2:
-    st.markdown("## Últimas mensagens")
+    st.markdown("## Últimas Normas")
 
     data = supabase.table("messages") \
         .select("*") \
@@ -88,18 +88,18 @@ with col2:
             dt_br = dt.astimezone(br_tz)
             dt_formatado = dt_br.strftime("%d/%m/%Y %H:%M")
 
-            texto1 = row.get("text", "")
+            norma_valor = row.get("Norma", "")
             texto2_valor = row.get("texto2", "")
 
             st.markdown(
                 f"""
                 <div style="padding:10px; border:1px solid #ddd; border-radius:5px; margin-bottom:10px;">
                     <strong>Data:</strong> {dt_formatado}<br>
-                    <strong>Texto 1:</strong> {texto1}<br>
+                    <strong>Norma:</strong> {norma_valor}<br>
                     <strong>Texto 2:</strong> {texto2_valor if texto2_valor else "<em>(vazio)</em>"}
                 </div>
                 """,
                 unsafe_allow_html=True
             )
     else:
-        st.info("Nenhuma mensagem ainda.")
+        st.info("Nenhuma norma cadastrada.")
